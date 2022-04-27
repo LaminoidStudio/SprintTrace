20ea917a16b68a9dbf90d289157699ac3be6e630
Benedikt Müssig <github@bmuessig.eu>
2022-04-27 11:21:02 +0200

    Added the primitive types

---
+++ libsprintpcb/primitives.c
@@ -0,0 +1,6 @@
+//
+// Created by Benedikt on 26.04.2022.
+// Copyright Laminoid.com (Muessig & Muessig GbR)
+//
+
+#include "primitives.h"
---
+++ libsprintpcb/primitives.h
@@ -0,0 +1,50 @@
+//
+// Created by Benedikt on 26.04.2022.
+// Copyright Laminoid.com (Muessig & Muessig GbR)
+//
+
+#ifndef SPRINTPCB_PRIMITIVES_H
+#define SPRINTPCB_PRIMITIVES_H
+
+typedef enum {
+    // The top copper layer (C1)
+    SPRINT_LAYER_COPPER_TOP = 1,
+
+    // The top silkscreen layer (S1)
+    SPRINT_LAYER_SILKSCREEN_TOP,
+
+    // The bottom copper layer (C2)
+    SPRINT_LAYER_COPPER_BOTTOM,
+
+    // The bottom silkscreen layer (S2)
+    SPRINT_LAYER_SILKSCREEN_BOTTOM,
+
+    // The first (usually top) inner copper layer (I1)
+    SPRINT_LAYER_COPPER_INNER1,
+
+    // The second (usually bottom) inner copper layer (I2)
+    SPRINT_LAYER_COPPER_INNER2,
+
+    // The mechanical outline layer (O)
+    SPRINT_LAYER_MECHANICAL
+} sprint_layer;
+
+typedef signed int sprint_dist;
+const sprint_dist SPRINT_DIST_PER_MM    = 10000;
+const sprint_dist SPRINT_DIST_MAX       = 500 * SPRINT_DIST_PER_MM;
+const sprint_dist SPRINT_DIST_MIN       = -SPRINT_DIST_MAX;
+
+typedef signed int sprint_angle;
+const sprint_angle SPRINT_ANGLE_WHOLE   = 1;
+const sprint_angle SPRINT_ANGLE_COARSE  = 100;
+const sprint_angle SPRINT_ANGLE_FINE    = 1000;
+const sprint_angle SPRINT_ANGLE_NATIVE  = SPRINT_ANGLE_FINE;
+const sprint_angle SPRINT_ANGLE_MAX     = 360 * SPRINT_ANGLE_NATIVE;
+const sprint_angle SPRINT_ANGLE_MIN     = -SPRINT_DIST_MAX;
+
+typedef struct {
+    int x;
+    int y;
+} sprint_tuple;
+
+#endif //SPRINTPCB_PRIMITIVES_H
