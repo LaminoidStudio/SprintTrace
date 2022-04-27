d54002961b2c42bd6613e67aa87b4af1f6cf9134
Benedikt Müssig <github@bmuessig.eu>
2022-04-27 19:28:06 +0200

    Added min and max functions

---
+++ libsprintpcb/math.h
@@ -0,0 +1,14 @@
+//
+// Created by Benedikt on 27.04.2022.
+// Copyright 2022, Laminoid.com (Muessig & Muessig GbR).
+// Licensed under the terms and conditions of the GPLv3.
+//
+
+#ifndef SPRINTPCB_MATH_H
+#define SPRINTPCB_MATH_H
+
+int imin(int a, int b);
+
+int imax(int a, int b);
+
+#endif //SPRINTPCB_MATH_H
