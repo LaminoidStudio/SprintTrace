e1273ce534cd4f1fc72a0fb24efb652a79c72e27
Benedikt Müssig <github@bmuessig.eu>
2022-04-27 11:20:23 +0200

    Implemented an arraylist

---
+++ libsprintpcb/list.c
@@ -0,0 +1,132 @@
+//
+// Created by Benedikt on 26.04.2022.
+// Copyright Laminoid.com (Muessig & Muessig GbR)
+//
+
+#include "errors.h"
+#include "list.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+sprint_list* sprint_list_create(int size, int capacity)
+{
+    if (size < 1 || capacity < 0) return NULL;
+
+    sprint_list* list = calloc(1, sizeof(*list));
+    list->size = size;
+    list->capacity = capacity;
+    list->elements = capacity > 0 ? malloc(capacity * size) : NULL;
+
+    return list;
+}
+
+sprint_error sprint_list_destroy(sprint_list* list)
+{
+    if (list == NULL) return SPRINT_ERROR_ARGUMENT_NULL;
+
+    list->count = 0;
+    list->capacity = 0;
+
+    if (list->elements != NULL)
+        free(list->elements);
+
+    free(list);
+    return SPRINT_ERROR_NONE;
+}
+
+sprint_error sprint_list_add(sprint_list* list, void* element)
+{
+    if (list == NULL) return SPRINT_ERROR_ARGUMENT_NULL;
+
+    // Start with an initial capacity of one
+    if (list->capacity < 1) list->capacity = 1;
+
+    // If the list is not initialized or the capacity is insufficient, grow the list
+    if (list->elements == NULL || list->count + 1 >= list->capacity) {
+        // When growing the list, double its count every time it has to be expanded
+        int new_capacity = list->capacity << 1;
+        if (new_capacity < list->capacity) return SPRINT_ERROR_OVERFLOW;
+        sprint_error error = sprint_list_grow(list, new_capacity);
+        if (error != SPRINT_ERROR_NONE) return error;
+    }
+
+    // Copy the new element to the end of the list and increment the count
+    memcpy(list->elements + list->size * list->count, element, list->size);
+    list->count++;
+    return SPRINT_ERROR_NONE;
+}
+
+void* sprint_list_get(sprint_list* list, int index)
+{
+    if (list == NULL || index < 0 || index >= list->count) return NULL;
+    return list->elements + list->size * index;
+}
+
+sprint_error sprint_list_set(sprint_list* list, int index, void* element)
+{
+    if (list == NULL) return SPRINT_ERROR_ARGUMENT_NULL;
+    if (index < 0 || index >= list->count) return SPRINT_ERROR_ARGUMENT_RANGE;
+
+    memcpy(list->elements + list->size * index, element, list->size);
+    return SPRINT_ERROR_NONE;
+}
+
+void* sprint_list_remove(sprint_list* list)
+{
+    if (list == NULL || list->count < 1) return NULL;
+
+    list->count--;
+    return list->elements + list->size * list->count;
+}
+
+sprint_error sprint_list_grow(sprint_list* list, int capacity)
+{
+    if (list == NULL) return SPRINT_ERROR_ARGUMENT_NULL;
+    if (capacity < 1) return SPRINT_ERROR_ARGUMENT_RANGE;
+
+    // If the list is uninitialized, initialize it
+    if (list->elements == NULL) {
+        list->elements = malloc(list->size * capacity);
+        if (list->elements == NULL) return SPRINT_ERROR_OVERFLOW;
+
+        list->capacity = capacity;
+        return SPRINT_ERROR_NONE;
+    }
+
+    // If the list is already big enough, do nothing
+    if (list->capacity >= capacity) return SPRINT_ERROR_NONE;
+
+    // Grow the list to the new count
+    void* new_elements = realloc(list->elements, list->size * capacity);
+    if (new_elements == NULL) return SPRINT_ERROR_OVERFLOW;
+
+    // Update the capacity and elements
+    list->capacity = capacity;
+    list->elements = new_elements;
+    return SPRINT_ERROR_NONE;
+}
+
+sprint_error sprint_list_trim(sprint_list* list)
+{
+    if (list == NULL) return SPRINT_ERROR_ARGUMENT_NULL;
+
+    // If there are no elements, trim the list to zero
+    if (list->elements == NULL) {
+        list->count = 0;
+        list->capacity = 0;
+        return SPRINT_ERROR_NONE;
+    }
+
+    // Only do something, if the count doesn't already match the capacity
+    if (list->count == list->capacity) return SPRINT_ERROR_NONE;
+
+    // Shrink the list to the count
+    void* new_elements = realloc(list->elements, list->size * list->count);
+    if (new_elements == NULL) return SPRINT_ERROR_OVERFLOW;
+
+    // Update the capacity and elements
+    list->capacity = list->count;
+    list->elements = new_elements;
+    return SPRINT_ERROR_NONE;
+}
---
+++ libsprintpcb/list.h
@@ -0,0 +1,42 @@
+//
+// Created by Benedikt on 26.04.2022.
+// Copyright Laminoid.com (Muessig & Muessig GbR)
+//
+
+#ifndef SPRINTPCB_LIST_H
+#define SPRINTPCB_LIST_H
+
+#include "errors.h"
+
+// Represents a growing list similar to ArrayList in Java
+typedef struct {
+    // The number of elements in this list
+    int count;
+
+    // The size of one element in bytes
+    int size;
+
+    // The total capacity of this list in elements
+    int capacity;
+
+    // The pointer to the elements of this list
+    void* elements;
+} sprint_list;
+
+sprint_list* sprint_list_create(int width, int capacity);
+
+sprint_error sprint_list_destroy(sprint_list* list);
+
+sprint_error sprint_list_add(sprint_list* list, void* element);
+
+void* sprint_list_get(sprint_list* list, int index);
+
+sprint_error sprint_list_set(sprint_list* list, int index, void* element);
+
+void* sprint_list_remove(sprint_list* list);
+
+sprint_error sprint_list_grow(sprint_list* list, int capacity);
+
+sprint_error sprint_list_trim(sprint_list* list);
+
+#endif //SPRINTPCB_LIST_H
