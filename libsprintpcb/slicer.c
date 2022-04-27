5140df599200eb4c0994b2047352b281ddf3b73c
Benedikt Müssig <github@bmuessig.eu>
2022-04-27 11:20:06 +0200

    Added slicer files and added special character consts

---
+++ libsprintpcb/slicer.c
@@ -0,0 +1,5 @@
+//
+// Created by Benedikt on 26.04.2022.
+//
+
+#include "slicer.h"
---
+++ libsprintpcb/slicer.h
@@ -0,0 +1,16 @@
+//
+// Created by Benedikt on 26.04.2022.
+//
+
+#ifndef SPRINTPCB_SLICER_H
+#define SPRINTPCB_SLICER_H
+
+const char SPRINT_STATEMENT_SEPARATOR = ',';
+const char SPRINT_STATEMENT_TERMINATOR = ';';
+const char SPRINT_VALUE_SEPARATOR = '=';
+const char SPRINT_TUPLE_SEPARATOR = '/';
+const char SPRINT_STRING_DELIMITER = '|';
+const char* SPRINT_TRUE_VALUE = "true";
+const char* SPRINT_FALSE_VALUE = "false";
+
+#endif //SPRINTPCB_SLICER_H
