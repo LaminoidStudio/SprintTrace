46802e14959c460aad00922b3fa57878b39d6e43
Benedikt Müssig <benedikt@gmx.info>
2022-04-29 14:48:40 +0200

    Added the new prints to the demo

--- main.c
+++ main.c
@@ -20,9 +20,13 @@ int main() {
 
     sprint_prim_format format_dist = SPRINT_PRIM_FORMAT_DIST_UM;
     sprint_prim_format format_angle = format_dist == SPRINT_PRIM_FORMAT_RAW ? format_dist : SPRINT_PRIM_FORMAT_COOKED;
+    sprint_prim_format format_layer = format_angle;
+
 
     sprint_stringbuilder* builder = sprint_stringbuilder_of("Circle and builder test:\n");
-    sprint_stringbuilder_format(builder, "layer: %d\n", circle.circle.layer);
+    sprint_stringbuilder_put_str(builder, "layer: ");
+    sprint_layer_string(circle.circle.layer, builder, format_layer);
+    sprint_stringbuilder_put_chr(builder, '\n');
 
     sprint_stringbuilder_put_str(builder, "width: ");
     sprint_dist_string(circle.circle.width, builder, format_dist);
@@ -36,9 +40,17 @@ int main() {
     sprint_dist_string(circle.circle.radius, builder, format_dist);
     sprint_stringbuilder_put_chr(builder, '\n');
 
-    sprint_stringbuilder_format(builder, "(clear): %d\n", circle.circle.clear);
-    sprint_stringbuilder_format(builder, "(cutout): %d\n", circle.circle.cutout);
-    sprint_stringbuilder_format(builder, "(soldermask): %d\n", circle.circle.soldermask);
+    sprint_stringbuilder_put_str(builder, "clear: ");
+    sprint_dist_string(circle.circle.clear, builder, format_dist);
+    sprint_stringbuilder_put_chr(builder, '\n');
+
+    sprint_stringbuilder_put_str(builder, "(cutout): ");
+    sprint_bool_string(circle.circle.cutout, builder);
+    sprint_stringbuilder_put_chr(builder, '\n');
+
+    sprint_stringbuilder_put_str(builder, "(soldermask): ");
+    sprint_bool_string(circle.circle.soldermask, builder);
+    sprint_stringbuilder_put_chr(builder, '\n');
 
     sprint_stringbuilder_put_str(builder, "(start): ");
     sprint_angle_string(circle.circle.start, builder, format_angle);
@@ -48,8 +60,16 @@ int main() {
     sprint_angle_string(circle.circle.stop, builder, format_angle);
     sprint_stringbuilder_put_chr(builder, '\n');
 
-    sprint_stringbuilder_format(builder, "(fill): %d\n\n", circle.circle.fill);
+    sprint_stringbuilder_put_str(builder, "(fill): ");
+    sprint_bool_string(circle.circle.fill, builder);
+    sprint_stringbuilder_put_chr(builder, '\n');
     sprint_element_destroy(&circle);
+
+    sprint_stringbuilder_put_str(builder, "String test: ");
+    sprint_str_string("my string raw", builder, SPRINT_PRIM_FORMAT_RAW);
+    sprint_stringbuilder_put_chr(builder, ' ');
+    sprint_str_string("my string cooked", builder, SPRINT_PRIM_FORMAT_COOKED);
+    sprint_stringbuilder_put_chr(builder, '\n');
 
     sprint_stringbuilder_flush(builder, stdout);
 
