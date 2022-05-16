94c934fdbf76e03de23f0d6f92feceaa5cd9297c
Benedikt Müssig <github@bmuessig.eu>
2022-05-17 01:51:10 +0200

    Corrected the copyright in output

--- libsprintpcb/output.c
+++ libsprintpcb/output.c
@@ -1,5 +1,7 @@
 //
 // Created by Benedikt on 17.05.2022.
+// Copyright 2022, Laminoid.com (Muessig & Muessig GbR).
+// Licensed under the terms and conditions of the GPLv3.
 //
 
 #include "output.h"
